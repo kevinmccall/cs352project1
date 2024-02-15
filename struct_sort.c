2c0525a982965e9b7c6c599c02176ba142688988
kevinmccall <erakiproj99@gmail.com>
2024-02-14 22:02:36 -0500

    did my share

--- struct_sort.c
+++ struct_sort.c
@@ -1,11 +1,35 @@
-#include <stdio.h>
 #include "./struct_sort.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 #define NUM_PEOPLE 100
+
 person_t data[NUM_PEOPLE];
 char str[100];
 
-int main (int argc, char **argv){
-    FILE *file = fopen(argv[1], "r");
+int main(int argc, char **argv) {
+    char *filename = "contacts.txt";
+    FILE *file = fopen(filename, "r");
+    // reads a single line in from the file
     fgets(str, 100, file);
+    // parse the string to create a struct
+    // 7
+    person_t person;
+    sscanf(str, "%[^,], %[^,], %[^,], %[^,], %[^,], %[^,], %s",
+           person.first_name, person.last_name, person.addy.address,
+           person.addy.city, person.addy.state, person.addy.zip_code,
+           person.phone_number);
+
+    printf("%s\n", person.first_name);
+
+    // printf("%s~ %s~ %s~ %s~ %s~ %s~ %s~\n", first_name, last_name, address,
+    //        city, state, zip, phone_num);
+
+    // add the struct to the array
+
+    // sort the array
+
+    // write to output file
+
+    // done :D
 }
--- struct_sort.h
+++ struct_sort.h
@@ -5,10 +5,20 @@
 #define CITY_SIZE 100
 #define FIRST_NAME 100
 #define LAST_NAME 100
+#define ZIP_SIZE 6
+#define PHONE_SIZE 13
 
-typedef struct {char address[ADDRESS_SIZE]; char city[CITY_SIZE]; char state[STATE];
-                int zip_code;} address;
-typedef struct {char first_name[FIRST_NAME]; char last_name[LAST_NAME];
-                address addy; int phone_number;} person_t;
+typedef struct {
+    char address[ADDRESS_SIZE];
+    char city[CITY_SIZE];
+    char state[STATE];
+    char zip_code[ZIP_SIZE];
+} address;
+typedef struct {
+    char first_name[FIRST_NAME];
+    char last_name[LAST_NAME];
+    address addy;
+    char phone_number[PHONE_SIZE];
+} person_t;
 
 #endif
